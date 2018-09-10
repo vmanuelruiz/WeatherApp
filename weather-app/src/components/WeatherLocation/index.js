2b2fd95eb3a9e54971a43b579206dc9c7eba1e07
vmanuelruiz <vmruiz.ruiz@gmail.com>
2018-09-10 09:00:28 +0200

    cambio 1

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
-import {SUN} from './../../constants/weathers';
+import {SUN, WINDY} from './../../constants/weathers';
 import './styles.css';
 
 const data = {
@@ -11,13 +11,41 @@ const data = {
     wind: '10 m/s',
 };
 
+const data2 = {
+    temperature: 15,
+    weatherState: WINDY,
+    humidity: 20,
+    wind: '10 m/s',
+};
+
 class WeatherLocation extends Component { 
-    render = () =>(
+
+    constructor(){ //siempre se debe invocar al componente base con super
+        super();
+        this.state = {
+            city: "Buenos Aires",
+            data: data,
+        };
+    }
+
+    handleUpdateClic = () => {
+        console.log('actualizado');
+        this.setState({
+            city: 'Buenos Aires!',
+            data: data2,
+        });
+    };
+
+    render = () =>{
+        const {city, data} = this.state;
+        return (
         <div className='weatherLocationCont'>
-            <Location city={'Granada!'}></Location>
+            <Location city={city}></Location>
             <WeatherData data={data}></WeatherData>
+            <button onClick={this.handleUpdateClic}>Actualizar</button>
         </div>
-    );
+        );
+    };
 };
 
 export default WeatherLocation;
