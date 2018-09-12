f1ab11909582c954dcb0d44049301e293e3aac0e
vmanuelruiz <vmruiz.ruiz@gmail.com>
2018-09-12 16:55:19 +0200

    cambio 29: aplicando el reducer

--- weather-app/src/store/index.js
+++ weather-app/src/store/index.js
@@ -1,6 +1,13 @@
 import {createStore} from 'redux';
+import {city} from './../reducers/city';
 
-export const store = createStore(() => {}, 
+const initialState = {
+    city: 'Buenos Aires,ar'
+};
+
+
+
+export const store = createStore(city, initialState,
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); //pasamos un reducer como parametro, q en este caso le pasamos una funcion vacia..
 //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 //este parametro es para enlazar REDUX con el pluggin de chrome...
